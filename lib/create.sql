359dc0ae5f0fddac8dfaea11078702d1f27b193a
Nicole D <nnardella@gmail.com>
2018-12-26 01:48:01 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE bears (
         id INTEGER PRIMARY KEY,
                 name TEXT,
-                age INTEGER
+                age INTEGER,
                 gender TEXT,
                 color TEXT,
                 temperament TEXT,
